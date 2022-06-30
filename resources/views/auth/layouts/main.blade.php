2ad50ee7039caead7ef04c445bb273b9d2e4d260
413x1 <muhammad.sholeh@sirclo.com>
2022-06-30 08:28:54 +0700

    layouting login page

---
+++ resources/views/auth/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    @include('auth.layouts.header')
+
+<body>
+    <div id="auth">
+        
+<div class="row h-100">
+    <div class="col-lg-5 col-12">
+        <div id="auth-left">
+
+            @yield('content')
+
+        </div>
+    </div>
+    <div class="col-lg-7 d-none d-lg-block">
+        <div id="auth-right">
+
+        </div>
+    </div>
+</div>
+
+    </div>
+</body>
+
+</html>
