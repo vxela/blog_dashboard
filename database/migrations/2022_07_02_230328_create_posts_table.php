a97dc4d0eccd2a33f5916ed569114c24b7009fa7
413x1 <muhammad.sholeh@sirclo.com>
2022-07-03 07:14:09 +0700

    add simple database plan and model

---
+++ database/migrations/2022_07_02_230328_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('bg_title');
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->enum('status', ['draft', 'editing', 'ready']);
+            $table->datetime('published_at');
+            $table->unsignedInteger('published_by_id');
+            $table->unsignedInteger('created_by_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
