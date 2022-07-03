e2033fc5af5e2a8a0413821850f7860b07a19603
413x1 <muhammad.sholeh@sirclo.com>
2022-07-03 10:26:57 +0700

    add tempelate clean blog and layouting

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 require __DIR__.'/auth.php';
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
 
 Route::middleware(['auth', 'role:admin|super|reader'])->group(function () {
